7ace451f7a1e4f5e6c5522c464a29ce0b3681f7c
Estephany <esurcoa@unsa.edu.pe>
2020-02-05 12:24:09 -0500

    is a final proyect

---
+++ js/home.js
@@ -0,0 +1,48 @@
+   function pagPaginas() {
+    document.getElementById("iframe").src="paginas.html";
+   }
+   function pagCapitulos() {
+    document.getElementById("iframe").src="capitulos.html";
+   }
+
+   function pagDigital() {
+    document.getElementById("iframe").src="digital.html";
+   }
+   function pagFisico() {
+    document.getElementById("iframe").src="fisicos.html";
+   }
+  function iconoMenuMasHorario() {
+    document.getElementById("horario").style.display = "block";
+    document.getElementById("mas").style.display = "none";
+    document.getElementById("menos").style.display = "inline";
+  }
+  function iconoMenuMenosHorario() {
+    document.getElementById("horario").style.display = "none";
+    document.getElementById("mas").style.display = "inline";
+    document.getElementById("menos").style.display = "none";
+  }
+  function iconoMenuMasCompra() {
+    document.getElementById("compra").style.display = "block";
+    document.getElementById("mas1").style.display = "none";
+    document.getElementById("menos1").style.display = "inline";
+  }
+  function iconoMenuMenosCompra() {
+    document.getElementById("compra").style.display = "none";
+    document.getElementById("mas1").style.display = "inline";
+    document.getElementById("menos1").style.display = "none";
+  }
+  function blog1() {
+    document.getElementById("iframe").src="la_magia_de_la_lectura.html";
+   }
+  
+  function blog2() {
+    document.getElementById("iframe").src="leer_nos_cambia_el_cerebro.html";
+   }
+  
+  function blog3() {
+    document.getElementById("iframe").src="las_cronicas_de_un_libro.html";
+   }
+
+  function blog4() {
+    document.getElementById("iframe").src="luchemos_contra_la_ignorancia.html";
+   }
